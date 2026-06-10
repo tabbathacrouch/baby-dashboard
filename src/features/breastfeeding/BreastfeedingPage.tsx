8dc636d878bb0e5c6a8cbcbc5313bc28535156d3
tabbatha <tabbatha@tabbathas-MacBook-Pro.local>
2026-06-10 10:36:29 -0500

    create folder structure

---
+++ src/features/breastfeeding/BreastfeedingPage.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export default function BreastfeedingPage() {
+  return <Typography variant="h4">Breastfeeding</Typography>;
+}
---
+++ src/features/diapers/DiapersPage.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export default function DiapersPage() {
+  return <Typography variant="h4">Diapers</Typography>;
+}
